4bb24fff2de15163a31b6f12fe17e11f8db78d37
Kevin <kevinsadao@hotmail.com>
2021-02-24 00:36:53 -0300

    Criado migration, model, controller e rota do usuário

--- aulas/api/src/server.ts
+++ aulas/api/src/server.ts
@@ -1,23 +1,12 @@
 import 'reflect-metadata';
 import express, { response } from 'express';
 import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-// http://localhost:3333/users
-
-app.get("/", (request, response) => {
-  // return response.send("Hello World - NLW04");
-  return response.json({message: "Hello World - NLW04"});
-});
-
-// 1 param => Rota(Recurso API)
-// 2 param => request, response
-
-app.post("/", (request, response) => {
-  // Recebeu os dados para salvar
-  return response.json({message: "Os dados foram salvos com sucesso!"});
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
 
